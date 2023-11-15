3a72b1fc3034820c229d93fef51bc069d1b11a52
Delfina8 <githubdelfina@gmail.com>
2023-11-15 16:59:56 -0300

    File Writer and FileReader

---
+++ ioexceptionarquivos/file/FileReaderTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.ioexceptionarquivos.file;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderTest01 {
+    public static void main(String[] args) {
+        File file = new File("fileTest2.txt");
+
+        try (FileReader fr = new FileReader(file)) {
+            int i;
+            while ((i = fr.read()) != -1) {
+                System.out.print((char) i);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
--- ioexceptionarquivos/file/FileWriter01.java
+++ ioexceptionarquivos/file/FileWriter01.java
@@ -10,11 +10,11 @@ import java.io.IOException;
 // BufferedReader
 public class FileWriter01 {
     public static void main(String[] args) {
-        File file = new File("fileTest2");
+        File file = new File("fileTest2.txt");
 
         try (FileWriter fw = new FileWriter(file)) {
 
-            fw.write("Teste de escrita");
+            fw.write("Teste de escrita \nTeste 2 escrita");
 
             fw.flush();
         } catch (IOException e){
